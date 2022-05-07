99e52edad9d4c37cdab197c5347f65b2018b3f69
lifyxus <lifyxus@gmail.com>
2022-05-07 13:51:45 +0300

    Added new method to make text link

--- Discord.Net.Extensions/Builders/EmbedBuilderExtensions.cs
+++ Discord.Net.Extensions/Builders/EmbedBuilderExtensions.cs
@@ -5,6 +5,19 @@
     /// </summary>
     public static class EmbedBuilderExtensions
     {
-        // TODO
+        /// <summary>
+        /// Adds link to the text to the <see cref="EmbedBuilder"/> in the description.
+        /// </summary>
+        /// <param name="embed">The <see cref="EmbedBuilder"/> this text link should be added to.</param>
+        /// <param name="text">Text which will be the link.</param>
+        /// <param name="url">The URL this <see cref="text"/>.</param>
+        /// <returns>The same <see cref="EmbedBuilder"/> with the text link.</returns>
+        public static EmbedBuilder AddTextLink(this EmbedBuilder embed, string text, string url)
+        {
+            if (!embed.Description.Contains(text))
+                throw new InvalidOperationException($"The description does not have this text ({text})");
+
+            return embed.WithDescription(embed.Description.Replace(text, $"[{text}]({url})"));
+        }
     }
 }
