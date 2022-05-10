2cce8f1b3f32b2c836a22cf83ff6687ee4aa03c4
Armano den Boef <t.a.denboef@gmail.com>
2022-05-10 17:06:58 +0200

    Init

---
+++ Samples/FactoryExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Extensions;
+using Discord.WebSocket;
+using FactoryExample;
+using Microsoft.Extensions.DependencyInjection;
+
+var factorySettings = new BuilderSettings<EmbedBuilder>(x =>
+{
+    x.AddField("Look:", "This field will always appear!");
+});
+
+var discordSettings = new DiscordSocketConfig()
+{
+    AlwaysDownloadUsers = true,
+    GatewayIntents = GatewayIntents.AllUnprivileged,
+};
+
+using var services = new ServiceCollection()
+    .AddSingleton(discordSettings)
+    .AddSingleton<DiscordSocketClient>()
+    .AddSingleton(factorySettings)
+    .AddSingleton<EmbedBuilderFactory>()
+    .AddSingleton<ReusableEmbedSender>()
+    .BuildServiceProvider();
+
+await services.GetRequiredService<ReusableEmbedSender>()
+    .StartAsync(1ul); // fill in a channel ID here.
---
+++ Samples/FactoryExample/ReusableEmbedSender.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Discord.Extensions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExample
+{
+    internal class ReusableEmbedSender
+    {
+        private readonly EmbedBuilderFactory _factory;
+        private readonly DiscordSocketClient _client;
+        private readonly System.Timers.Timer _timer;
+
+        private ITextChannel _channel = null!;
+
+        public ReusableEmbedSender(EmbedBuilderFactory factory, DiscordSocketClient client)
+        {
+            _factory = factory;
+            _timer = new()
+            {
+                Interval = 10000,
+                AutoReset = true
+            };
+            _client = client;
+        }
+
+        public async Task StartAsync(ulong channelId)
+        {
+            _timer.Elapsed += OnElapse;
+            _timer.Start();
+
+            var channel = await _client.GetChannelAsync(channelId);
+            if (channel is not ITextChannel textChannel)
+                throw new ArgumentException($"Channel with id {channelId} not found.", nameof(channelId));
+            else
+                _channel = textChannel;
+        }
+
+        private async void OnElapse(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            var eb = _factory.Generate();
+
+            eb.WithDescription("Test");
+
+            await _channel.SendMessageAsync(embed: eb.Build());
+        }
+    }
+}
