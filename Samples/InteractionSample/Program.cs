dd805e9cd5f0adcf8b80313c32dee0ddfa04aa92
Armano den Boef <t.a.denboef@gmail.com>
2022-05-10 17:31:10 +0200

    Add another sample

---
+++ Samples/InteractionSample/Program.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+var discordSettings = new DiscordSocketConfig()
+{
+    AlwaysDownloadUsers = true,
+    GatewayIntents = GatewayIntents.AllUnprivileged,
+};
+
+var interactionSettings = new InteractionServiceConfig()
+{
+    UseCompiledLambda = true,
+    DefaultRunMode = RunMode.Async
+};
+
+using var services = new ServiceCollection()
+    .AddSingleton(discordSettings)
+    .AddSingleton<DiscordSocketClient>()
+    .AddSingleton(interactionSettings)
+    .AddSingleton<InteractionService>()
+    .BuildServiceProvider();
+
+var interactionService = services.GetRequiredService<InteractionService>();
+
+interactionService.AddModulesAsync(typeof(Program).Assembly, services);
+
+var discordClient = services.GetRequiredService<DiscordSocketClient>();
+
+discordClient.Ready += async () =>
+{
+    await interactionService.RegisterCommandsGloballyAsync(true);
+};
+
+discordClient.InteractionCreated += async (i) =>
+{
+    var ctx = Discord.Extensions.Interactions.InteractionContext.GenerateGeneric(i, discordClient);
+
+    await interactionService.ExecuteCommandAsync(ctx, services);
+};
+
+discordClient.Log += LogAsync;
+interactionService.Log += LogAsync;
+
+Task LogAsync(LogMessage arg)
+{
+    Console.WriteLine(arg);
+    return Task.CompletedTask;
+}
---
+++ Samples/InteractionSample/SlashCommandModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractionSample
+{
+    public class SlashCommandModule : InteractionModuleBase<SocketInteractionContext<SocketSlashCommand>>
+    {
+        [SlashCommand("ping", "Pong")]
+        public async Task PingAsync()
+            => await RespondAsync("pong");
+    }
+}
